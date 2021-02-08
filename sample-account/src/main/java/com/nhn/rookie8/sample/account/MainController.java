ecd541f0e60d8744390f32c5a97fe8ad7c793e5f
seongbinLim <i1709i10@gmail.com>
2021-02-08 16:24:49 +0900

    sample account

--- sample-account/src/main/java/com/nhn/rookie8/sample/account/MainController.java
+++ sample-account/src/main/java/com/nhn/rookie8/sample/account/MainController.java
@@ -3,15 +3,22 @@ package com.nhn.rookie8.sample.account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String welcome(Model model) {
         model.addAttribute("time", new Date());
         model.addAttribute("message", "account api");
         return "welcome";
+    }
+
+    @GetMapping("/helloworld")
+    @ResponseBody //just string, view resolver, message converter
+    public String hellowworld() {
+        return "adf nnnn Hello Rookie!";
     }
 }
